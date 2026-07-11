b079671b217a975a1aec7f761f932cd3d629bd89
MiaHenstridge <nongngocmai22@gmail.com>
2026-07-11 17:46:43 +0200

    tested with different user question

--- 02_agent_loop_raw_function_calling.py
+++ 02_agent_loop_raw_function_calling.py
@@ -167,4 +167,4 @@ def run_agent(question: str):
 if __name__ == "__main__":
     print("Hello Langchain Agent (.bind_tools)")
     print()
-    result = run_agent("What is the price of a keyboard after applying a gold discount?")
+    result = run_agent("What is the price of a keycap set after applying a silver discount?")
